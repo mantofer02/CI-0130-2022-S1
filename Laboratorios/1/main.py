d9d0ed97ea2a0c1c8cee7feb9fd1b422913743de
Marco Ferraro <mantofer2000@gmail.com>
2022-05-02 12:18:27 -0600

    Parser

---
+++ Laboratorios/1/main.py
@@ -0,0 +1,31 @@
+import parser
+import sys
+
+# def main():
+#     print(f"Arguments: {sys.argv}")
+    
+#     if (len(sys.argv) < 2):
+#         file_reader = FileReader()
+#     else:
+#         file_reader = FileReader(sys.argv[1])
+#         if (sys.argv[-1] == '-v'):
+#             file_reader.verbose_mode()
+
+# def main():
+#     if(len(sys.argv) == 1):
+#         equation = input()
+#         # print(equation)
+#         n = input("Ingrese cantidad de restricciones")
+#         restriction = []
+#         while(n != 0):
+#             restriction.append(input())
+#             n -= 1
+#     else:
+#         with open('equation.txt') as f:
+#             lines = f.readlines()
+
+def main():
+  print(parser.parse_equation("-3.8x1 + 5x2 -2x3 + x4 -9x5"))
+
+if __name__ == "__main__":
+    main()
---
+++ Laboratorios/1/parser.py
@@ -0,0 +1,22 @@
+
+# @param:
+#   equation: -3.8x1 + 5x2 -2x3
+# @output: 'x1': -3.8, 'x2': 5.0, 'x3': -2.0
+def parse_equation(equation):
+    dict = {}
+    equation = equation.replace("+", " ")
+    i = 1
+    while equation:
+      var = "x" + str(i)
+      split = equation.split(var, 1)
+      value = split.pop(0)
+      value = value.replace(" ", "")
+      if value == '':
+        value = "1"
+      elif value == '-':
+        value = "-1"
+      
+      dict[var] = value.split()[0]
+      equation = split[0]
+      i += 1
+    return dict
