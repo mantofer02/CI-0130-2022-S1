084a889dfd78a0c90cc8799773620b7a082f79ff
mantofer02 <mantofer02@gmail.com>
2022-05-07 16:50:36 -0600

    definyng zj row

--- Laboratorios/1/math_simplex.py
+++ Laboratorios/1/math_simplex.py
@@ -6,3 +6,14 @@ def simplex(objective, restrictions, variables, maximize):
 
   restrictions_np = np.array(restrictions)
   restrictions_np = restrictions_np.astype(np.float64)
+
+  # c array is not always 0
+  c_array = np.zeros(len(restrictions))
+  zj_array = np.zeros(len(variables))
+  z_minus_zj_array = np.zeros(len(variables))
+
+
+  for i in range(len(restrictions_np)):
+    multip_array = np.multiply(c_array, restrictions_np[:, i])
+    zj_array[i] = np.sum(multip_array)
+
