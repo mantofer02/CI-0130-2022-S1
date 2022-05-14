5ecfdd7d12021849fd4123e9de43a9d79c281a9e
Marco Ferraro <mantofer2000@gmail.com>
2022-05-14 12:12:24 -0600

    Iterating simplex

--- Laboratorios/1/math_simplex.py
+++ Laboratorios/1/math_simplex.py
@@ -13,24 +13,37 @@ def simplex(objective, restrictions, variables, maximize):
     restrictions_np = restrictions_np.astype(np.float64)
 
     # c array is not always 0
-    c_array = np.zeros(len(restrictions))
-    var_array = init_var_col(variables)
+    var_col = init_var_col(variables)
+    c_col = init_c_col(var_col, objective, variables)
     # usa la expndida
-    zj_array = np.zeros(len(variables) + 1)
+    zj_array = np.zeros(len(variables))
     z_minus_zj_array = np.zeros(len(variables))
 
-    for i in range(len(variables) + 1):
-        multip_array = np.multiply(c_array, restrictions_np[:, i])
+    for i in range(len(variables)):
+        multip_array = np.multiply(c_col, restrictions_np[:, i])
         zj_array[i] = np.sum(multip_array)
 
     z_minus_zj_array = objective[:] - zj_array[:]
-    # print(z_minus_zj_array)
 
-    if (not is_solution(z_minus_zj_array, maximize)):
+    # an iteration
+    while (not is_solution(z_minus_zj_array, maximize)):
         pivot_col = get_pivot_col(z_minus_zj_array, maximize)
-        # vlaidate
-        pivot_row = get_pivot_row(restrictions[:, pivot_col])
-        pass
+        pivot_row = get_pivot_row(restrictions_np[:, pivot_col], restrictions_np[:, len(variables)])
+        restrictions_np[pivot_row, :] = restrictions_np[pivot_row, :] / restrictions[pivot_row][pivot_col]
+
+        for i in range(len(restrictions_np)):
+            if i is not pivot_row:
+                restrictions_np[i, :] = (-1*restrictions_np[i][pivot_col]* restrictions_np[pivot_row, :]) + restrictions_np[i, :]
+
+        var_col[pivot_row] = variables[pivot_col]
+        c_col[pivot_row] = objective[pivot_col]
+
+          
+        for i in range(len(variables)):
+            multip_array = np.multiply(c_col, restrictions_np[:, i])
+            zj_array[i] = np.sum(multip_array)
+
+        z_minus_zj_array = objective[:] - zj_array[:]
 
 
 def is_solution(z_minus_zj_array, maximize):
@@ -53,8 +66,8 @@ def get_pivot_col(z_minus_zj_array, maximize):
         return np.where(z_minus_zj_array == np.amin(z_minus_zj_array))[0][0]
 
 
-def get_pivot_row(column, b_row):
-    ratio_row = np.copy(b_row)
+def get_pivot_row(column, b_col):
+    ratio_row = np.copy(b_col)
     index = 0
     min_value = MAX_VAL
     for i in range(len(ratio_row)):
@@ -69,22 +82,25 @@ def get_pivot_row(column, b_row):
 def init_var_col(variables):
     var_col = []
     for var in variables:
-      if len(var.split("s")) > 1:
-        var_col.append(var)
-      if len(var.split("a")) > 1:
-        index = int(var.split("a")[1])
-        var_col[index - 1] = var
+        if len(var.split("s")) > 1:
+            var_col.append(var)
+        if len(var.split("a")) > 1:
+            index = int(var.split("a")[1])
+            var_col[index - 1] = var
     return var_col
+
 
 def init_c_col(var_col, objective, variables):
     var_col_index = 0
     c_array = np.zeros(len(var_col))
     for i in range(len(variables)):
         if variables[i] == var_col[var_col_index]:
-          c_array[var_col_index] = objective[i]
-          var_col_index += 1
+            c_array[var_col_index] = objective[i]
+            var_col_index += 1
     return c_array
 
-get_pivot_row([2, 3, 1], [400, 300, 90])
-var_col = init_var_col(["x1", "x2", "s1", "s2", "s3", "a3"])
-print(init_c_col(var_col, [30.0, 100.0, 0, 0, 0, -10000], ["x1", "x2", "s1", "s2", "s3", "a3"]))
+def fill_z_array():
+  pass
+# get_pivot_row([2, 3, 1], [400, 300, 90])
+# var_col = init_var_col(["x1", "x2", "s1", "s2", "s3", "a3"])
+# print(init_c_col(var_col, [30.0, 100.0, 0, 0, 0, -10000], ["x1", "x2", "s1", "s2", "s3", "a3"]))
