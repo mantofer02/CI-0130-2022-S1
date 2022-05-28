7cf95a419bfd578cffd05d28d3aeae9d29742aa9
Marco Ferraro <mantofer2000@gmail.com>
2022-05-27 20:31:07 -0600

    Add files | Starting lab 2

---
+++ Laboratorios/2/model.py
@@ -0,0 +1,15 @@
+
+def calculate_transitions(words, squences):
+    pass
+
+
+def create_model(words, ngrams):
+    pass
+
+
+def generate_model(model, seed):
+    pass
+
+
+def get_probability(model, word):
+    pass
