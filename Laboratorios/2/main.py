897419dd62bc6629a9e8d88f724bab7bf6f7a3fc
gabogantes <gabriel.bogantesarmijo@ucr.ac.cr>
2022-05-29 15:59:48 -0600

    Added get_sequences finished

--- Laboratorios/2/main.py
+++ Laboratorios/2/main.py
@@ -1,7 +1,12 @@
 import sys
+import parser_csv
 
 
 def main():
+    # parser_csv.load_words('pokemon.csv')
+    #print(parser_csv.add_decorators(parser_csv.load_words('pokemon.csv'), "$", 2))
+    print(parser_csv.get_sequences(parser_csv.add_decorators(
+        parser_csv.load_words('pokemon.csv'), "$", 2), 3))
     pass
 
 
