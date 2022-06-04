873cb37066020e39a0cc3db65593f34efdd535b3
Marco Ferraro <mantofer2000@gmail.com>
2022-06-03 22:44:24 -0600

    Add transition matrix problem

--- Laboratorios/2/main.py
+++ Laboratorios/2/main.py
@@ -4,15 +4,16 @@ import model
 
 
 def main():
-    n = 3
+    n = 1
     decorator = '$'
 
-    words = parser_csv.load_words('test.csv')
+    words = parser_csv.load_words('names.csv')
     words_with_decorators = parser_csv.add_decorators(
         words, decorator=decorator, n=n)
 
     sequences = parser_csv.get_sequences(words=words_with_decorators, n=n)
-    model.calculate_transitions(sequences=sequences, words=sequences)
+    model.calculate_transitions(
+        words=words_with_decorators, sequences=sequences)
 
 
 if __name__ == "__main__":
