fefa820b35db24e33dc72e77d180c221ab9389ef
mantofer02 <mantofer02@gmail.com>
2022-06-26 11:09:11 -0600

    adding queue

--- Laboratorios/3/client.py
+++ Laboratorios/3/client.py
@@ -2,9 +2,27 @@ class Client:
     def __init__(self):
         self.queue_time = 0
         self.served_time = 0
+        self.waiting = True
+        self.finished = False
+
+    def finish(self):
+        self.finish = True
+
+    def is_waiting(self):
+        return self.waiting
+
+    def go_to_server(self):
+        self.waiting = False
 
     def increment_queue_time(self):
         self.queue_time += 1
 
     def increment_serverd_time(self):
         self.server_time += 1
+
+    def tick(self):
+        if (not self.finished):
+            if self.waiting:
+                self.increment_queue_time()
+            else:
+                self.increment_serverd_time()
--- Laboratorios/3/server.py
+++ Laboratorios/3/server.py
@@ -4,17 +4,27 @@ from client import Client
 class Server:
     def __init__(self):
         self.current_client = None
+        self.serving_time = 0
+        self.free_time = 0
+        self.finished = False
 
     def working(self):
         if self.current_client:
             return True
         return False
 
-    def increment_queue_time(self):
-        self.queue_time += 1
+    def increment_serving_time(self):
+        self.serving_time += 1
 
-    def increment_serverd_time(self):
-        self.server_time += 1
+    def increment_free_time(self):
+        self.free_time += 1
 
     def add_client(self, client):
         self.current_client = client
+
+    def tick(self):
+        if (not self.finished):
+            if self.working():
+                self.increment_serving_time()
+            else:
+                self.increment_free_time()
