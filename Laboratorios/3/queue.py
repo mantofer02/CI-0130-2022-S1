8fd1e0417866661e7dbb1d0d4502196d7169c2cb
Marco Ferraro <mantofer2000@gmail.com>
2022-07-03 12:16:13 -0600

    system functionality

--- Laboratorios/3/queue.py
+++ Laboratorios/3/queue.py
@@ -12,14 +12,15 @@ class Queue:
         self.mu = mu
 
         self.clients_on_wait: List[Client] = []
-        self.clients_on_servers: List[Client] = []
+        self.clients_on_servers: List[Client] = [None] * self.s
+        self.clients_finished: List[Client] = []
         self.servers: List[Server] = []
         self.time = 0
         self.clients_served = 0
         self.clients_lost = 0
 
         self.init_servers()
-        self.simulation(30)
+        self.simulation(1000)
 
     def init_servers(self):
         for i in range(self.s):
@@ -27,36 +28,60 @@ class Queue:
 
     def tick(self):
         self.time += 1
-        for i in self.clients_on_wait:
-            i.tick()
+        for i in range(len(self.clients_on_wait)):
+            self.clients_on_wait[i].tick()
 
-        for i in self.clients_on_servers:
-            i.tick()
+        for i in range(len(self.clients_on_servers)):
+            if (self.clients_on_servers[i]):
+                self.clients_on_servers[i].tick()
 
         for i in self.servers:
             i.tick()
 
     def simulation(self, time_limit):
+        lambd_counter = 0
         while (self.time < time_limit):
+            # print("Lambda value = " + str(self.lambd(self.get_n())))
+            if (lambd_counter >= self.lambd(self.get_n())):
+                self.add_client_to_queue()
+                lambd_counter = 0
 
-            self.clients_on_wait.append(Client())
+            for i in range(len(self.servers)):
+                if (self.servers[i].serving_time >= self.lambd(self.get_n())):
+                    self.free_client_from_server(i)
 
-            for server in self.servers:
-                if (server.working() == False):
+            for i in range(len(self.servers)):
+                if (self.servers[i].working() == False):
                     if (len(self.clients_on_wait) > 0):
-
+                        print("client on server!")
                         client = self.clients_on_wait.pop(0)
                         client.set_wait(False)
-                        server.add_client(client)
-                        self.clients_on_servers.append(client)
+                        self.servers[i].add_client(client)
+                        self.clients_on_servers[i] = client
 
             self.tick()
+            lambd_counter += 1
+        print(self.time)
+        input()
 
-            for i in self.clients_on_servers:
-                print("Time of client " + str(i.served_time))
+    def get_n(self):
+        n = 0
+        for client in self.clients_on_servers:
+            if (client):
+                n += 1
 
-            print("Finished after " + str(self.time))
-            input()
+        return len(self.clients_on_wait) + n
+
+    def add_client_to_queue(self):
+        if (self.get_n() <= self.lmax):
+            print("client added to queue!")
+            self.clients_on_wait.append(Client())
+
+    def free_client_from_server(self, i):
+        self.clients_finished.append(self.servers[i].free_server())
+        self.clients_on_servers[i] = None
+        print("client freed from system!")
+        self.clients_served += 1
 
     def print_results(self):
         pass
