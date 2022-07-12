03f745be7a2cfc368933c0f27119786fdfa0b17b
Marco Ferraro <mantofer2000@gmail.com>
2022-07-11 23:02:30 -0600

    changing numbers

--- Laboratorios/4/main.py
+++ Laboratorios/4/main.py
@@ -6,7 +6,7 @@ import game_util
 
 
 def main():
-    abm = random_util.good_abm(10000)
+    abm = random_util.good_abm(10000000)
 
     random_generator = CongruentialGenerator(a=abm[0], b=abm[1], m=abm[2])
     print("a value: " + str(abm[0]))
@@ -22,7 +22,8 @@ def main():
         player_cards_numbers.append(index)
         stack.pop(index)
 
-    results = game_util.simulate(player_cards_numbers, 40, random_generator)
+    results = game_util.simulate(
+        player_cards_numbers, 100000, random_generator)
     print("Test case 1: ")
     print("Wins %: " + str(results[0] * 100))
     print("Lose %: " + str(results[2] * 100))
@@ -35,8 +36,25 @@ def main():
         player_cards_numbers.append(index)
         stack.pop(index)
 
-    results = game_util.simulate(player_cards_numbers, 40, random_generator)
+    print()
+    results = game_util.simulate(
+        player_cards_numbers, 100000, random_generator)
     print("Test case 2: ")
+    print("Wins %: " + str(results[0] * 100))
+    print("Lose %: " + str(results[2] * 100))
+
+    stack = game_util.generate_cards_stack()
+    player_cards_numbers = []
+
+    for i in range(2):
+        index = int(random_generator.random() * len(stack))
+        player_cards_numbers.append(index)
+        stack.pop(index)
+
+    print()
+    results = game_util.simulate(
+        player_cards_numbers, 100000, random_generator)
+    print("Test case 3: ")
     print("Wins %: " + str(results[0] * 100))
     print("Lose %: " + str(results[2] * 100))
 
