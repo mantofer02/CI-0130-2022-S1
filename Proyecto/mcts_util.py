0b96185c3bc41242d4eb5fcb27432e697ed964a1
Marco Ferraro <mantofer2000@gmail.com>
2022-07-22 22:22:36 -0600

    parent

--- Proyecto/mcts_util.py
+++ Proyecto/mcts_util.py
@@ -8,12 +8,16 @@ class Node(object):
     def __init__(self, state):
         self.state: Quarto = state
         self.children = []
+        self.parent: Quarto = None
         self.explored = False
         # AI WINS/ PLAYER WINS
         self.result = [0, 0]
 
     def add_child(self, obj):
         self.children.append(obj)
+
+    def set_parent(self, parent):
+        self.parent = parent
 
 
 def mcts(root: Quarto, time_limit=0.25, exploitation=0.5):
@@ -30,6 +34,7 @@ def mcts(root: Quarto, time_limit=0.25, exploitation=0.5):
 def expand_node(node: Node, unexplored_nodes):
     for action in node.state.get_available_actions():
         child = Node(node.state.do_action(action))
+        child.set_parent(node)
         node.add_child(child)
         unexplored_nodes.append(child)
 
