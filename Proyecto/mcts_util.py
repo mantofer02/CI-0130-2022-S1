3358a81740228f7c403a52bf8433f9cd13cb9527
Gabriel Revillat <gabrielrevillat@yahoo.es>
2022-07-23 23:42:24 -0600

    ¿Será?

--- Proyecto/mcts_util.py
+++ Proyecto/mcts_util.py
@@ -13,6 +13,7 @@ class Node(object):
         self.parent: Node = None
         self.explored = False
         self.id = id
+        self.leads_to_one = False
         # AI WINS/ PLAYER WINS
         self.result = [0, 0]
 
@@ -28,45 +29,97 @@ def mcts(root: Quarto, time_limit=0.25, exploitation=0.5):
     # unexplored_nodes = []
     # explored_nodes = []
 
-    current_node = tree_root
-    while(not current_node.state.has_finished()):
-        rand_value = random.random()
-        if (current_node.explored == True and rand_value > exploitation):
-            """
-            Si este estado ha sido explorado antes, entonces genere un número
-            al azar, de ser mayor al exploitation entonces se elige la acción
-            que lleva al mejor estado conocido
-            """
-            
-        else:
-            current_node.explored = True
-            current_node = expand_node(current_node)
+    count = 10
+    while (count > 0):
+        current_node = tree_root
+        while(not current_node.state.has_finished()):
+            rand_value = random.random()
+            if (current_node.explored and rand_value > exploitation):
+                """
+                Si este estado ha sido explorado antes, entonces genere un número
+                al azar, de ser mayor al exploitation entonces se elige la acción
+                que lleva al mejor estado conocido
+                """
+                # current_node = find_best_state(current_node)
+                index = 0
+                chosen = False
+                while (not chosen and index < len(current_node.children)):
+                    current_child = current_node.children[index]
+                    if (current_child.result == [1, 0]):
+                        current_node = current_child
+                        chosen = True
+                    else:
+                        index += 1
+            else:
+                current_node.explored = True
+                current_node = expand_node(current_node)
 
-    # Si en el estado final gana la IA
-    if current_node.state.get_winner() == AI_WIN:
-        current_node.result = [1, 0]
+        # Si en el estado final gana la IA
+        if current_node.state.get_winner() == AI_WIN:
+            current_node.result = [1, 0]
 
-        end = False
-        while (current_node is not tree_root and not end):
-            for child in current_node.parent.children:
-                if (child.result == [0, 1]):
-                    end = True
-                else:
+            while (current_node != tree_root):
+                current_node.leads_to_one = True
+
+                if (ai_wins(current_node)):        
                     current_node.parent.result = current_node.result
-                    current_node = current_node.parent
+                    
+                current_node = current_node.parent
 
-    else:
-        current_node.result = [0, 1]
+        else:
+            current_node.result = [0, 1]
 
-        while (current_node is not tree_root):
-            current_node.parent.result = current_node.result
-            current_node = current_node.parent
+            while (current_node != tree_root):
+                current_node.parent.result = current_node.result
+                current_node = current_node.parent
+
+        count -= 1
 
     # aqui se selecciona a un hijo random
     # select_node(tree_root.children[0], explored_nodes)
 
+    current_node = tree_root
+    print(f"SOY RAIZ {current_node.result}")
+    if (current_node.result != [1, 0]):
+        current_node = find_best_state(current_node)
+
+    return current_node
+
 # Return none is it has all children, so it does not expand
 
+def ai_wins(current_node: Node):
+    for child in current_node.parent.children:
+        if (child.result == [0, 1]):
+            return False
+
+    return True
+
+def find_best_state(current_node: Node):
+    """
+
+    """
+
+    index = 0
+    chosen = False
+    result_node: Node = current_node
+    while (not chosen and index < len(current_node.children)):
+        current_child = current_node.children[index]
+        if (current_child.result == [1, 0]):
+            print("Sirve")
+            result_node = current_child
+            chosen = True
+        else:
+            if (current_child.leads_to_one):
+                print("Llevo a 1.")
+                result_node = current_child
+                chosen = True
+                """result_node = find_best_state(current_child)
+                if (result_node.result == [1, 0]):
+                    chosen = True"""
+
+        index += 1
+
+    return result_node
 
 def expand_node(node: Node):
     actions = node.state.get_available_actions()
@@ -94,6 +147,14 @@ def get_duplicate(node: Node, child: Node):
         if i.id == child.id:
             return i
 
-
+"""
 root = Node(Quarto())
 expand_node(root)
+"""
+
+def run():
+    result = mcts(Quarto())
+    print(result.result)
+
+if __name__ == "__main__":
+    run()
